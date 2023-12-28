a3c6e4fe27f462d3bbc61d5e90424915d9a0b0fd
moessj6 <mt@web-labels.de>
2023-12-28 11:06:06 +0100

    initial

---
+++ Weblabels/src/Weblabels.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Weblabels;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class Weblabels extends Plugin implements ThemeInterface
+{
+}
